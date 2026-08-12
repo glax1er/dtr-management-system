6d834cbb6f3ef25460984a0a034d2c5d924bfb3b
Mikki <ambutlang@gmail.com>
2026-08-13 00:04:31 +0800

    remove unused codes

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['role', 'name', 'email', 'password', 'notifications_cleared_at'])]
+#[Fillable(['role', 'name', 'email', 'password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
@@ -53,7 +53,6 @@ class User extends Authenticatable implements PasskeyUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
-            'notifications_cleared_at' => 'datetime',
         ];
     }
 
